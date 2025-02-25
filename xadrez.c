018d5f6bd7387ac2b53a7e190cdf55b723c473a4
johnnyrcn <99361737+johnnyrcn@users.noreply.github.com>
2025-02-25 01:09:25 +0000

    Desafio Xadrez Nível MESTRE

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+void movimentacao(int casas, char *movimento){
+    if (casas > 0){
+        printf("Andou 1 casa para %s! \n", movimento);
+        movimentacao(casas - 1, movimento);
+    }
+}
+
 int main(){
 
-    int peca, i = 0, casa;
-    char direcao;
-
+    int peca, i = 0, casa, movimento, escolha;
+    char direcao[4][20] = {"Cima", "Baixo", "Esquerda", "Direita"};
+    char direcaoBispo[4][50] = {"Diagonal Cima Esquerda", "Diagonal Cima Direita", "Diagonal Baixo Esquerda", "Diagonal Baixo Direita"};
 do
 {
     printf("\nEscolha qual peça deseja mover: \n");
@@ -23,42 +30,29 @@ case 1:
         scanf("%d", &casa);
         getchar();
             printf("Escolha para qual direção deseja andar: \n");
-            printf("a. Diagonal Cima Esquerda\n");
-            printf("b. Diagonal Cima Direita\n");
-            printf("c. Diagonal Baixo Esquerda\n");
-            printf("d. Diagonal Baixo Direita\n");
-            scanf("%c", &direcao);
+            printf("1. Diagonal Cima Esquerda\n");
+            printf("2. Diagonal Cima Direita\n");
+            printf("3. Diagonal Baixo Esquerda\n");
+            printf("4. Diagonal Baixo Direita\n");
+            scanf("%d", &escolha);
 
-
-                switch (direcao)
+                switch (escolha)
                 {
-                case 'a':
-                    while (i < casa)
-                    {
-                    printf("Bispo se movimentou 1 casa na diagonal cima esquerda!\n");
-                    i++;
-                    }
+                case 1:
+                    printf("\nMovimentação do Bispo: \n");
+                    movimentacao(casa, direcaoBispo[0]);
                     break;
-                case 'b':
-                    while (i < casa)
-                {
-                    printf("Bispo se movimentou 1 casa na diagonal cima direita!\n");
-                    i++;
-                }
+                case 2:
+                    printf("\nMovimentação do Bispo: \n");
+                    movimentacao(casa, direcaoBispo[1]);
                 break;
-                case 'c':
-                    while (i < casa)
-                {
-                    printf("Bispo se movimentou 1 casa na diagonal baixo esquerda!\n");
-                    i++;
-                }
+                case 3:
+                    printf("\nMovimentação do Bispo: \n");
+                    movimentacao(casa, direcaoBispo[2]);
                 break;
-                case 'd':
-                    while (i < casa)
-                {
-                    printf("Bispo se movimentou 1 casa na diagonal baixo direita!\n");
-                    i++;
-                }
+                case 4:
+                    printf("\nMovimentação do Bispo: \n");
+                    movimentacao(casa, direcaoBispo[3]);
                 break;
                 default:
                     printf("Opção Inválida!\n");
@@ -70,44 +64,30 @@ case 2:
     printf("\nDigite quantas casas desejar andar\n");
     scanf("%d", &casa);
     getchar();
-                printf("Escolha para qual direção deseja andar: \n");
-                printf("a. Cima \n");
-                printf("b. Baixo \n");
-                printf("c. Esquerda \n");
-                printf("d. Direita \n");
+                printf("\nEscolha para qual direção deseja andar: \n");
+                printf("1. Cima \n");
+                printf("2. Baixo \n");
+                printf("3. Esquerda \n");
+                printf("4. Direita \n");
                 printf("Opção: ");
-                scanf("%c", &direcao);
-                    switch (direcao)
+                scanf("%d", &escolha);
+                    switch (escolha)
                     {
-                    case 'a':
-                    {
-                        do
-                        {
-                            printf("Torre andou 1 casa para cima! \n");
-                            i++;
-                        } while (i < casa);
-                        }
+                    case 1:
+                        printf("\nMovimentação da Torre: \n");
+                        movimentacao(casa, direcao[0]);
                         break;
-                    case 'b':
-                        do
-                        {
-                            printf("Torre andou 1 casa para baixo! \n");
-                            i++;
-                        } while (i < casa);
+                    case 2:
+                        printf("\nMovimentação da Torre: \n");
+                        movimentacao(casa, direcao[1]);
                         break;
-                    case 'c':
-                        do
-                        {
-                            printf("Torre andou 1 casa para esquerda! \n");
-                            i++;
-                        } while (i < casa);
+                    case 3:
+                        printf("\nMovimentação da Torre: \n");
+                        movimentacao(casa, direcao[2]);
                         break;
-                    case 'd':
-                        do
-                        {
-                            printf("Torre andou 1 casa para direita! \n");
-                            i++;
-                        } while (i < casa);
+                    case 4:
+                        printf("\nMovimentação da Torre: \n");
+                        movimentacao(casa, direcao[3]);
                         break;
                     default:
                     printf("Opção Inválida!\n");
@@ -120,73 +100,79 @@ case 3:
     scanf("%d", &casa);
     getchar();
                 printf("Escolha para qual direção deseja andar: \n");
-                printf("a. Cima \n");
-                printf("b. Baixo \n");
-                printf("c. Esquerda \n");
-                printf("d. Direita \n");
+                printf("1. Cima \n");
+                printf("2. Baixo \n");
+                printf("3. Esquerda \n");
+                printf("4. Direita \n");
                 printf("Opção: ");
-                scanf("%c", &direcao);
-            switch (direcao)
+                scanf("%d", &escolha);
+            switch (escolha)
         {
-            case 'a':
-                for(i = 0; i < casa; i++)
-                {
-                    printf("Rainha andou 1 casa para cima! \n");
-                } break;
-            case 'b':
-                for(i = 0; i < casa; i++)
-                {
-                    printf("Rainha andou 1 casa para baixo! \n");
-                } break;
-            case 'c':
-                for(i = 0; i < casa; i++)
-                {
-                    printf("Rainha andou 1 casa para esquerda!! \n");
-                } break;
-            case 'd':
-                for(i = 0; i < casa; i++)
-                {
-                    printf("Rainha andou 1 casa para direita! \n");
-                } break;
+            case 1:
+                printf("\nMovimentação da Rainha: \n");
+                movimentacao(casa, direcao[0]);
+                break;
+            case 2:
+                printf("\nMovimentação da Rainha: \n");
+                movimentacao(casa, direcao[1]);
+                break;
+            case 3:
+                printf("\nMovimentação da Rainha: \n");
+                movimentacao(casa, direcao[2]);
+                break;
+            case 4:
+                printf("\nMovimentação da Rainha: \n");
+                movimentacao(casa, direcao[0]);
+                break;
             }
     break;
 
 case 4: {
 
-int movimento1, movimento2, i = 1, j = 1;
-char direcao1[20], direcao2[20];
+int movimento1, movimento2, direcao1, direcao2;
+char direcao[4][30] = {"Cima", "Baixo", "Esquerda", "Direita"};
 
     printf("### Lembre-se que o cavalo só pode se mover fazendo um movimento que resulta em um L (deve totalizar 3 casas) ### \n");
     printf("Digite quantas casas deseja andar no primeiro movimento: ");
     scanf("%d", &movimento1);
-    printf("\nEscolha a direção na qual o cavalo se movimentará no primeiro movimento (Esquerda, Direita, Cima ou Baixo): ");
-    scanf("%s", direcao1);
+    printf("\nEscolha a direção na qual o cavalo se movimentará no primeiro movimento: \n");
+    printf("0. Cima \n");
+    printf("1. Baixo \n");
+    printf("2. Esquerda \n");
+    printf("3. Direita \n");
+    printf("Direção: ");
+    scanf("%d", &direcao1);
     getchar();
 
-    printf("\nDigite quantas casas deseja andar no segundo movimento: ");
-    scanf("%d", &movimento2);
-    printf("\nEscolha a direção na qual o cavalo se movimentará no segundo movimento (Esquerda, Direita, Cima ou Baixo): ");
-    scanf("%s", direcao2);
+    printf("\nEscolha a direção na qual o cavalo se movimentará no segundo movimento: \n");
+    printf("0. Cima \n");
+    printf("1. Baixo \n");
+    printf("2. Esquerda \n");
+    printf("3. Direita \n");
+    printf("Direção: ");
+    scanf("%d", &direcao2);
     getchar();
 
+    movimento2 = (movimento1 == 1) ? 2 : 1;
+    printf("\n");
         if ((movimento1 == 2 && movimento2 == 1) || (movimento1 == 1 && movimento2 == 2))
         {
-
-            for(i = 1; i <= movimento2; i++)
-            {
-                while (j <= movimento1)
-                {
-                printf("Cavalo se moveu 1 casa para %s!\n", direcao1);
-                j++;
-                }
-            printf("Cavalo se moveu 1 casa(s) para %s!\n", direcao2);
-             }
-        } else
-        {
+            for(int i = 0, j = 0; i < movimento2 || j < movimento1; )
+                if (j < movimento1) 
+                    {
+                        printf("Cavalo se moveu 1 casa para %s!\n", direcao[direcao1]);
+                        j++;
+                    } else if (i < movimento2)
+                    {
+                        printf("Cavalo se moveu 1 casa para %s!\n", direcao[direcao2]);
+                        i++;
+                    }
+    } else {
         printf("Movimento Inválido! O Cavalo DEVE andar 3 casas em formato de L, qualquer valor a mais ou a menos será invalidado.\n");
-        }
-    break;
     }
+    break;
+}
+
 case 5:
     printf("Saindo...\n");
     break;
@@ -194,8 +180,9 @@ case 5:
 default:
     printf("Opção Inválida!\n");
     break;
+    }
 
-}
+
 } while (peca < 4);
 
 return 0;
@@ -216,4 +203,4 @@ return 0;
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    // Inclua o uso de continue e break dentro dos loops
